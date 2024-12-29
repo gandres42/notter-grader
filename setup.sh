6a0d42645ad839e4caedaf44b8aebe139d0205fb
gandres42 <gandres2416@gmail.com>
2024-12-29 00:24:04 -0600

    condense into only required files, create autograder.zip on the fly using required files only

--- setup.sh
+++ setup.sh
@@ -1,8 +1,29 @@
-apt-get update
-apt-get install curl
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+# # install uv
+# apt-get update
+# apt-get install curl
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+# source $HOME/.local/bin/env
 
-uv python install 3.12.7
-uv init && rm hello.py README.md
-uv add numpy matplotlib scipy nbconvert otter-grader ipykernel
+# # install python
+# uv python install 3.12.7
+
+# delete env for testing purposes
+rm -rf .venv pyproject.toml uv.lock .python-version
+
+# # create environment
+# uv init && rm hello.py README.md
+# uv add numpy matplotlib scipy nbconvert otter-grader ipykernel
+
+# # move everything in ./files to ./, create autograder.zip, then move them back
+# moved_files=()
+# for file in ./files/*; do
+#     if [ -f "$file" ]; then
+#         mv "$file" ./ 
+#         moved_files+=("$(basename "$file")")
+#     fi
+# done
+# files_list=$(printf " %s" "${moved_files[@]}")
+# uv run otter generate $files_list
+# for file in "${moved_files[@]}"; do
+#     mv "./$file" ./files/
+# done
